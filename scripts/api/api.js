e3051b3cde8f017b4457ec2f1cf8b7145e2e0615
Vincent HAVET <vhavet@gmail.com>
2021-01-09 17:12:15 +0100

    Creation functions displayResultString, displaySearchString, request api axios

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -6,8 +6,9 @@ class LibraryApi {
      * @see https://jikan.docs.apiary.io/#introduction/information
      * @see https://github.com/axios/axios
      */
-    constructor(base_url) {
-        throw new Error("Not implemented")
+    constructor(base_url, resource) {
+        this.base_url = base_url;
+        this.resource = resource;
     }
 
     /**
@@ -16,7 +17,32 @@ class LibraryApi {
      * @returns {Promise} Promesse qui retourne les données JSON en cas de succès
      * @see https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Promise
      */
-    fetch(resource_url) {
-        throw new Error("Not implemented")
-    }
+    load = async (resource_url) => {
+
+        try {
+            const result = await axios.get(`${this.base_url}/search/anime?q=${resource_url}&page=1`);
+           
+            return result.data.results;
+
+        } catch(error) {console.log(error)};
+    } 
+
+    /**
+    * @todo Utiliser la méthode fetch de l'api. Construire resource_url avec les propriétés resource & query
+    * @param {string} query => peut être par exemple '/10087', '/anime?q=FateZero'
+    * @returns {Promise} Promesse qui retourne les données JSON en cas de succès
+    */
+    // loadList = async (query) => {
+
+    //     try {
+    //         const result = await axios.get(`${this.base_url}/search/${this.resource}?q=${query}&page=1`);
+
+    //         //console.log(result.data.results);
+           
+    //         return result.data.results;
+
+    //     } catch(error) {console.log(error)};
+    // } 
+    
+    
 }
