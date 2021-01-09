39a2f3f62d9ba6435b56ed5faf623de7d3d0b74c
Vincent HAVET <vhavet@gmail.com>
2021-01-09 17:26:19 +0100

    request api axios for display card modal

--- scripts/api/searchResource.js
+++ scripts/api/searchResource.js
@@ -1,11 +1,22 @@
 class SearchResource extends LibraryResource {
 
+    
+    constructor(api, resource) {
+        super(api, resource);
+    }
     /**
      * @param {string} name
      * @todo Utiliser l'api de la classe parente pour appeler la méthode fetch
      * @returns {Promise} Promesse qui retourne les données JSON en cas de succès
      */
-    byName(name) {
-        throw new Error("Not implemented")
+    byName = async (id) => {
+
+        try {
+            const result = await axios.get(`${this.api}/${this.resource}/${id}`);
+           
+            return result.data;
+
+        } catch(error) {console.log(error)};
+        
     }
 }
